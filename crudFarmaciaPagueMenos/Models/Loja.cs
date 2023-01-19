f425085e46d4c969780006094675aac565165ff7
Pedro Henrique Linhares <94800633+Pedrohla14@users.noreply.github.com>
2023-01-19 16:51:20 -0300

    alteracao em Loja

--- crudFarmaciaPagueMenos/Models/Loja.cs
+++ crudFarmaciaPagueMenos/Models/Loja.cs
@@ -1,14 +1,19 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace ProjetoPagueMenos.Models
 {
 
     public class Loja
     {
+        [Key]
         public int Id { get; set; }
 
         public string? Nome { get; set; }
 
         public string? Endereco { get; set; }
+
+        public string? Telefone { get; set; }
 
         public List<Produto>? Produtos { get; set; } 
     }
